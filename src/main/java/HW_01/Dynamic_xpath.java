94d8bf72031cf766df1ffbc5295dc96d0344f996
Shailamukta <shailakhanum7@gmail.com>
2023-11-06 00:02:07 -0500

    selenium

---
+++ src/main/java/HW_01/Dynamic_xpath.java
@@ -0,0 +1,22 @@
+package HW_01;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Dynamic_xpath {
+// Tag with a single attribute
+@FindBy(xpath="//button[@class='search-icon__container']")
+WebElement searchButton;
+// Tag with inner text
+@FindBy(xpath ="//button[text()='Resident']")
+WebElement residentButton;
+// Tag with partial inner text from any website.
+@FindBy(xpath="//a[contains(text(),'Texas by ')]")
+WebElement tByT;
+// Tag with multiple attributes with 'and' logic
+@FindBy(xpath ="//button[@class='main-nav__nav-button' and text()='Find Services']")
+WebElement findService;
+
+	}
+
+
